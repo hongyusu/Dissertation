ac42c560825bb7d8e20de6e0cc32ab21d25efa9d
hongyusu <hongyu.su.uh@gmail.com>
2014-11-05 13:37:57 +0200

    minor

--- autorun.sh
+++ autorun.sh
@@ -10,6 +10,10 @@ pdflatex $maintextname
 
 rm ${maintextname}.asc ${maintextname}.ase ${maintextname}.asp ${maintextname}.aux ${maintextname}.glo ${maintextname}.ilg ${maintextname}.ist ${maintextname}.log ${maintextname}.nlo ${maintextname}.nls ${maintextname}.toc ${maintextname}.blg
 
+pdflatex dissertation_abstract
+rm dissertation_abstract.aux
+rm dissertation_abstract.log
+
 echo 'Number of references:'
 cat ${maintextname}.bib|grep '@'|wc -l
 
